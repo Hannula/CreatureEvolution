a4cb3d0ef2e8ee1fde8fdf0ef3526b7169e07994
Hannula <hannula.rama.p@student.uta.fi>
2020-01-09 16:46:53 +0200

    Add tileset image file

--- Assets/Sandbox/Level.cs
+++ Assets/Sandbox/Level.cs
@@ -1,18 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Level : MonoBehaviour {
-    Vector2Int dimensions;
-    private Tile[,] tileGrid;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Sandbox
+{
+    public class Level : MonoBehaviour
+    {
+        Vector2Int dimensions;
+        private Tile[,] tileGrid;
+    }
 }
