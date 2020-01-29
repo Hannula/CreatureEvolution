5ecd243758260f6f96992dcc3561f6a9320795f6
Hannula <hannula.rama.p@student.uta.fi>
2020-01-29 23:02:51 +0200

    Fix tile raycast

--- Assets/Sandbox/TileInspector.cs
+++ Assets/Sandbox/TileInspector.cs
@@ -9,22 +9,25 @@ public class TileInspector : MonoBehaviour
     public Text tileElevationText;
     public Text tileTemperatureText;
 
+    public LayerMask raycastLayermask;
+
     public TileVisualizer selectedTile;
 
     void Update()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out hit);
-        selectedTile = hit.collider.transform.parent.GetComponent<TileVisualizer>();
-        
-        
+        Physics.Raycast(ray, out hit, raycastLayermask);
+        if (hit.collider != null)
+            selectedTile = hit.collider.transform.parent.GetComponent<TileVisualizer>();
+
+
 
         if (selectedTile)
         {
             tileNameText.text = selectedTile.tile.terrain.name;
-            tileElevationText.text = selectedTile.tile.elevation.ToString();
-            tileTemperatureText.text = selectedTile.tile.temperature.ToString();
+            tileElevationText.text = "Elevation: " + selectedTile.tile.elevation.ToString();
+            tileTemperatureText.text = "Temperature: " + selectedTile.tile.temperature.ToString();
         }
     }
 }
